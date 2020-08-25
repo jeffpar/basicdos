c854fbd16e80b9a6d2627931653feeb1af58eb9e
Jeff Parsons <Jeff@pcjs.org>
2020-08-25 15:15:48 -0700

    Updated TESTS.BAT

--- software/pcx86/src/util/tests.bat
+++ software/pcx86/src/util/tests.bat
@@ -1,8 +1,12 @@
-print 2+2
-print 1;2;3;4;5;6;7;8;9
-let a = 99
-print (-a)^2/3+&hff*(3+4)
+REM The first BASIC-DOS test file
+COLOR 7,1
+PRINT 2+2
+PRINT 1;2;3;4;5;6;7;8;9
+LET a = 99
+PRINT (-a)^2/3+&hff*(3+4)
 REM Result should be 5052
-print -(-a)^2/3+&hff*(3+44)
+PRINT -(-a)^2/3+&hff*(3+44)
 REM Result should be 8718
-print a,b,c,d,e,f,g,h,i
+REM
+REM String support is coming soon
+REM Until then, avoid using strings
