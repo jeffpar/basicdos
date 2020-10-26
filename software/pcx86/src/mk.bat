0ea56d449adbab6fbfe03a738ee05578b3f891eb
Jeff Parsons <Jeff@pcjs.org>
2020-10-26 09:39:32 -0700

    Updated MK batch files

--- software/pcx86/src/mk.bat
+++ software/pcx86/src/mk.bat
@@ -1,3 +1,4 @@
+ECHO OFF
 CD BOOT
 REM Use MK FINAL to create a non-debug release
 IF "%1"=="" MK DEBUG ALL
@@ -5,3 +6,5 @@ IF "%1"=="debug" MK DEBUG ALL
 IF "%1"=="DEBUG" MK DEBUG ALL
 IF "%1"=="final" MK FINAL ALL
 IF "%1"=="FINAL" MK FINAL ALL
+ECHO Usage: MK [DEBUG or FINAL]
+CD ..
