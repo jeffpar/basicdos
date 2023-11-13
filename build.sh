94423b4b3292ec7f0d0f0c6c834ecef67ac040c9
Jeff Parsons <Jeff@pcjs.org>
2023-11-13 12:23:37 -0800

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo This command builds drive D: with the BASIC-DOS source code.
-echo At the "C:\>" prompt, type "D:", then "MK", then "QUIT".
+echo This command creates a drive [D:] containing the BASIC-DOS source code.
+echo To build the source, at the "C:\>" prompt, type "D:", then "MK", then "QUIT".
 echo Any modifications will be written back to the software/pcx86/src directory.
 pc.js --disk=https://harddisks.pcjs.org/pcx86/10mb/MSDOS330-C400.json --dir=software/pcx86/src --normalize
