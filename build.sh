fd1df5d60ba121ef5d9c4ec571a33f29d65ca9a3
Jeff Parsons <Jeff@pcjs.org>
2026-02-01 10:57:01 -0800

    Updated build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# echo This command creates a drive [D:] containing the BASIC-DOS source code.
-# echo To build the source, at the "C:\>" prompt, type "D:", then "MK", then "QUIT".
-# echo Any modifications will be written back to the software/pcx86/src directory.
+#
+# This command creates a drive [D:] containing the BASIC-DOS source code.
+# To build the source, at the "C:\>" prompt, type "D:", then "MK", then "QUIT".
+# Any modifications will be written back to the software/pcx86/src directory.
+#
+# NOTE: While early BASIC-DOS builds were performed in a browser using a PCjs
+# PC XT with PC DOS 2.00, our command-line build environment uses PC.js with a
+# COMPAQ DeskPro 386 configuration running MS-DOS 3.30, in part because that
+# machine has a real-time clock that MS-DOS 3.30 knows how to use.
+#
+# pc.js --disk=software/pcx86/disks/PCDOS200-C400.json --dir=software/pcx86/src --normalize
+#
 pc.js --disk=https://harddisks.pcjs.org/pcx86/10mb/MSDOS330-C400.json --dir=software/pcx86/src --normalize
